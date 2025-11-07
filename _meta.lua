1d5f5d8154f91d0db7b1fa23489cba944310b37a
omer-faruq <85738646+omer-faruq@users.noreply.github.com>
2025-11-07 08:34:39 +0300

    v1.0.2

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "memobook",
     fullname = _([[Memo Book]]),
     description = _([[Personal memos for highlighted text selections.]]),
-    version = "1.0.1",
+    version = "1.0.2",
 }
