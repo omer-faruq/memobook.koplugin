2928716f821a31ce9d723c7ab44c22c5062283e5
omerfarukinel <85738646+omer-faruq@users.noreply.github.com>
2026-07-25 09:03:45 +0300

    v1.2.0

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "memobook",
     fullname = _([[Memo Book]]),
     description = _([[Personal memos for highlighted text selections.]]),
-    version = "1.1.0",
+    version = "1.2.0",
 }
